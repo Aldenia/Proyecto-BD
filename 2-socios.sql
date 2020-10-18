b2c5dac468e45b4870f2cea114f782dbfbf24c2a
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-10-18 17:57:45 -0600

    arreglando tablas

--- 2-socios.sql
+++ 2-socios.sql
@@ -16,7 +16,7 @@ Create Table socios
 	--PK
 	Constraint PK_voluntarios Primary key Clustered(socio_id),
 	--FK
-	Constraint FK_socios_cuota Foreign Key(tipo_cuota_fk) References tipo_cuota(couta_id),
-	Constraint FK_socios_sede Foreign Key(sede_fk) References sede(sede_id)
+	Constraint FK_socios_cuota Foreign Key(tipo_cuota_fk) References tipo_cuotas(couta_id),
+	Constraint FK_socios_sede Foreign Key(sede_fk) References sedes(sede_id)
 )
 Go
