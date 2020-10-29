333fa2342e42269dfa45186dc867d1ef02a2025b
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-10-28 23:24:55 -0600

    creacion de procedimiento almacenado borrar socio y buscar socio

--- MainFileGroup/5-SP_socios.sql
+++ MainFileGroup/5-SP_socios.sql
@@ -31,3 +31,37 @@ As
 	Inner Join tipo_cuotas cuotas On cuotas.cuota_id = socios.tipo_cuota_fk
 
 Go
+
+--busca los socios existentes
+Create Procedure buscar_socios @nombre Varchar(25)
+
+As
+	Select 
+	sedes.sede_id,
+	sedes.nombre_sede,
+	socios.socio_id, 
+	socios.nombre, 
+	socios.apellido1, 
+	socios.apellido2, 
+	socios.direccion, 
+	cuotas.cuota_id, 
+	cuotas.tipo, 
+	cuotas.cantidad, 
+	socios.cuanta_bancaria, 
+	socios.fecha_pago  
+	from socios
+	Inner Join sedes On sedes.sede_id = socios.sede_fk
+	Inner Join tipo_cuotas cuotas On cuotas.cuota_id = socios.tipo_cuota_fk
+	where socios.nombre = @nombre
+
+Go
+
+--borra socio existente
+Create Procedure borrar_socio @socio_id int
+
+As
+	Delete from socios
+	Where socios.socio_id = @socio_id
+
+Go
+
