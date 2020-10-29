4990830ccff6f4e2514e10eb37b893faaf640343
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-10-28 23:10:17 -0600

    prodedimiento almacenado listar socios creado

--- MainFileGroup/5-SP_socios.sql
+++ MainFileGroup/5-SP_socios.sql
@@ -1,10 +1,33 @@
 Use ACNUR
 Go
-
+--Insertar en tabla socios
 Create Procedure insertar_socio @nombre Varchar(25), @apellido1 Varchar(25), @apellido2 Varchar(25), 
 								@direccion Varchar(200), @fecha_pago Date, @cuanta_bancaria Int,
 								@tipo_cuota_fk Int, @sede_fk Int
 As
 	Insert Into socios (nombre, apellido1, apellido2, direccion, fecha_pago, cuanta_bancaria, tipo_cuota_fk, sede_fk)
 	Values (@nombre, @apellido1, @apellido2, @direccion, @fecha_pago, @cuanta_bancaria, @tipo_cuota_fk,@sede_fk)
+Go
+
+--listar los socios existentes
+Create Procedure listar_socios
+
+As
+	Select 
+	sedes.sede_id,
+	sedes.nombre_sede,
+	socios.socio_id, 
+	socios.nombre, 
+	socios.apellido1, 
+	socios.apellido2, 
+	socios.direccion, 
+	cuotas.cuota_id, 
+	cuotas.tipo, 
+	cuotas.cantidad, 
+	socios.cuanta_bancaria, 
+	socios.fecha_pago  
+	from socios
+	Inner Join sedes On sedes.sede_id = socios.sede_fk
+	Inner Join tipo_cuotas cuotas On cuotas.cuota_id = socios.tipo_cuota_fk
+
 Go
