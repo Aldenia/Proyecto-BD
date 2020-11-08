7566da96aa07e0a63be3b011a6437797517902fc
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-11-07 19:02:04 -0600

    Sp sede mayor ingreso creada

--- 2-HeadFileGroup/8-Sp_sede_con_mayor_ingreso.sql
+++ 2-HeadFileGroup/8-Sp_sede_con_mayor_ingreso.sql
@@ -1,9 +1,23 @@
 Use ACNUR
 go
+--Stored procedure sedes con más ingresos
+Create Procedure sedes_mayor_ingresos
+	As
+		Select sedes.nombre_sede,SUM(tipo_cuotas.cantidad) Monto from socios
+		inner join sedes
+		on sedes.sede_id = socios.sede_fk
+		inner join tipo_cuotas
+		on tipo_cuotas.cuota_id = socios.tipo_cuota_fk
+		Group By sedes.nombre_sede
+		Order by Monto Desc
+	Go
 
-Select sedes.nombre_sede, SUM(tipo_cuotas.cantidad) As Monto from socios
-	inner join sedes
-	on sedes.sede_id = socios.sede_fk
-	inner join tipo_cuotas
-	on tipo_cuotas.cuota_id = socios.tipo_cuota_fk
-	Group By sedes.nombre_sede
+--Stored procedure cantidad de socios y sus cuotas
+Create procedure cantidad_socios_por_cuota
+	As
+		Select tipo_cuotas.tipo As cuota, Count(socios.socio_id) As cantidad_socios from socios
+		inner join tipo_cuotas
+		on tipo_cuotas.cuota_id = socios.tipo_cuota_fk
+		Group by tipo_cuotas.tipo
+		Order by cantidad_socios Desc
+	Go
